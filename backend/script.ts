1062a5e0419a9294ee866a06dda7c0c372cd11e9
veressgergo88 <veress.gergo@gmail.com>
2023-08-02 21:11:33 +0200

    base connect, inputs

--- backend/script.ts
+++ backend/script.ts
@@ -1,1 +1,26 @@
-console.log('Hello World!')
+import express from "express";
+import type { Request, Response } from "express";
+import cors from "cors";
+import { z } from "zod"
+
+const server = express();
+
+server.use(cors());
+
+const RequestSchema = z.object({
+    v1: z.string(),
+    v2: z.string()
+})
+
+server.get("/", (req: Request, res: Response) => {
+  
+  const result = RequestSchema.safeParse(req.query)
+  
+  if (!result.success) {
+    return res.sendStatus(400)
+  }
+
+  res.send("result is: " + result.data.v1 + result.data.v2);
+});
+
+server.listen(3333);
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,23 +2,14 @@ import "./style.css";
 import http from "axios";
 import { z } from "zod";
 
-const CharacterResponseSchema = z.object({
-  info: z.object({
-    next: z.string(),
-  }),
-  results: z
-    .object({
-      name: z.string(),
-    })
-    .array(),
-});
-type CharacterResponse = z.infer<typeof CharacterResponseSchema>;
+const ResponseSchema = z.string()
+type ResponseSchema = z.infer<typeof ResponseSchema>;
 
-const load = async (): Promise<CharacterResponse | null> => {
-  const response = await http.get("https://rickandmortyapi.com/api/character");
+const load = async (v1: string, v2:string): Promise<ResponseSchema | null> => {
+  const response = await http.get("http://localhost:3333",{ params: { "v1": v1, "v2":v2}})
   const data = response.data;
 
-  const result = CharacterResponseSchema.safeParse(data);
+  const result = ResponseSchema.safeParse(data);
 
   if (!result.success) {
     console.log(result.error);
@@ -29,9 +20,11 @@ const load = async (): Promise<CharacterResponse | null> => {
 };
 
 const init = async () => {
-  const characters = await load();
-  if (characters)
-    document.getElementById("app")!.innerHTML = characters.results[0].name;
+  const value1 = (document.getElementById("elso") as HTMLInputElement).value
+  const value2 = (document.getElementById("masodik") as HTMLInputElement).value
+  const str = await load(value1, value2);
+  if (str)
+    document.getElementById("app")!.innerHTML = str
 };
 
 document.getElementById("load-button")!.addEventListener("click", init);
