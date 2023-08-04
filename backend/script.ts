5201e333d26fc1cf9a60ccd1149e501956ba7d8f
veressgergo88 <veress.gergo@gmail.com>
2023-08-04 21:24:32 +0200

    Added two .txt file in the database folder, and wrote three GET script

--- backend/script.ts
+++ backend/script.ts
@@ -1,26 +1,72 @@
 import express from "express";
 import type { Request, Response } from "express";
 import cors from "cors";
-import { z } from "zod"
+import fs from "fs/promises";
+import { z } from "zod";
 
 const server = express();
 
 server.use(cors());
 
-const RequestSchema = z.object({
-    v1: z.string(),
-    v2: z.string()
-})
+type User = {
+  id: number;
+  name: string;
+  age: number;
+};
 
-server.get("/", (req: Request, res: Response) => {
-  
-  const result = RequestSchema.safeParse(req.query)
-  
-  if (!result.success) {
-    return res.sendStatus(400)
-  }
+const QueryParamSchema = z.object({
+  name: z.string(),
+});
 
-  res.send("result is: " + result.data.v1 + result.data.v2);
+const parse = (data: string): User[] =>
+  data
+    .split("\n")
+    .filter((row) => !!row)
+    .map((row) => ({
+      id: +row.split(",")[0],
+      name: row.split(",")[1],
+      age: +row.split(",")[2],
+    }));
+
+// REST API - GET (method) /api/users (path) => array
+/*server.get("/api/users", async (req: Request, res: Response) => {
+  const userData = await fs.readFile("./database/users.txt", "utf-8")
+  const response: User[] = parse(userData)
+  res.json(response)
+});
+*/
+
+// REST API - GET - (/api/users?name=John, /api/users?age=30&name=John) => array
+/*
+server.get("/api/users", async (req: Request, res: Response) => {
+  //validálunk
+  const result = QueryParamSchema.safeParse(req.query);
+  if (!result.success) return res.sendStatus(400);
+
+  const query = result.data;
+
+  //olvassuk a fájl adatait
+  const userData = await fs.readFile("./database/users.txt", "utf-8");
+  //az adatot átalakítjuk JSON formátumra
+  const users = parse(userData);
+  //szűrjük a name értéke alapján
+  const filteredUsers = users.filter((user) => user.name.includes(query.name));
+  //válasznak kiíratjuk a szűrt adatot
+  res.json(filteredUsers);
+});
+*/
+// REST API - GET /api/users/15 (id -ra szokás!!!) path variable => 1 object
+server.get("/api/users/:id", async (req: Request, res: Response) => {
+  const id = +req.params.id
+  
+  const userData = await fs.readFile("./database/users.txt", "utf-8");
+  const users = parse(userData);
+  let filteredUser = users.filter(user => user.id === id)
+  
+  if (!filteredUser)
+    return res.sendStatus(404)
+
+  res.json(filteredUser);
 });
 
 server.listen(3333);
